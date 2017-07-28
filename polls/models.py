49d0206142fc40508a2da6e24d5f7614e0040235
strkita <kita83@gmail.com>
2017-07-28 22:12:55 +0900

    second commit

--- polls/models.py
+++ polls/models.py
@@ -9,7 +9,12 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        # 昨日以降に作成されたものか比較する
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
     def __str__(self):
         return self.question_text
@@ -17,7 +22,7 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text =models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
